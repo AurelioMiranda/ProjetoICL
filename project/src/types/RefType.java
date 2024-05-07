41a12469fd000c1267c00ffe86fa95748304f53b
Francisco Ribeiro e Silva <far.silva@campus.fct.unl.pt>
2024-05-07 16:36:46 +0100

    retry Ref Types and Values

--- project/src/types/RefType.java
+++ project/src/types/RefType.java
@@ -1,2 +1,23 @@
-package types;public class RefType {
+package types;
+
+public class RefType {
+    private Type innerType;
+
+    public RefType(Type innerType) {
+        this.innerType = innerType;
+    }
+
+    public Type getInnerType() {
+        return innerType;
+    }
+
+    public boolean equals(Object obj) {
+        return this == obj;
+    }
+
+
+    @Override
+    public String toString() {
+        return "RefType(" + innerType.toString() + ")";
+    }
 }
--- project/src/values/RefValue.java
+++ project/src/values/RefValue.java
@@ -1,2 +1,27 @@
-package values;public class RefValue {
+package values;
+
+public class RefValue implements Value{
+    private Value value;
+
+    public RefValue(Value value) {
+        this.value = value;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    //fala sobre o valor da casa a referencia em si
+    public void setValue(Value value) {
+        this.value = value;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof RefValue && value == ((RefValue)obj).getValue();
+    }
+    @Override
+    public String toString() {
+        return "ref("+value.toString()+")";
+    }
+
 }
